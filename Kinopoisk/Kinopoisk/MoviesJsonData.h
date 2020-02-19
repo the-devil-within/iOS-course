19644a62a696efe779cfd80662d2370d7b7c56af
Evgeny <blinov@bpcbt.com>
2020-02-20 00:07:16 +0300

    fix file reference

---
+++ Kinopoisk/Kinopoisk/MoviesJsonData.h
@@ -0,0 +1,15 @@
+//
+//  MoviesJsonData.h
+//  tableViewExample
+//
+//  Created by Blinov on 16.02.2020.
+//  Copyright © 2020 Evgeny Blinov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MoviesJsonData : NSObject
+
+@property (nonatomic, strong) NSData *jsonData;
+
+@end
